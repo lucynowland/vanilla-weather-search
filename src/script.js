2e56a16baa1f3bb0ae94164d44bc51bdd7fb1752
Lucy Nowland <lucynowland03@gmail.com>
2023-11-09 14:38:14 +0000

    Added search functionality using the API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,27 @@
-function chnageCityName(event) {
+function updateWeatherData(response) {
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.city;
+
+  let tempElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  tempElement.innerHTML = Math.round(temperature);
+}
+
+function searchCityName(city) {
+  let apiKey = "b4695dbeo3231b4ta37cdcd77c20d1fa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
+function updateCityName(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = searchInput.value;
+  searchCityName(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", chnageCityName);
+searchFormElement.addEventListener("submit", updateCityName);
+
+searchCityName("Edinburgh");
