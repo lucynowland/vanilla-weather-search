dc1561a29628a20f48e2bca20a99ffd05838ffc3
Lucy Nowland <lucynowland03@gmail.com>
2023-11-09 15:36:21 +0000

    Injected weather conditions using the API
    Humidity
    Windspeed
    Time
    Weather Description

--- src/script.js
+++ src/script.js
@@ -5,6 +5,40 @@ function updateWeatherData(response) {
   let tempElement = document.querySelector("#current-temperature");
   let temperature = response.data.temperature.current;
   tempElement.innerHTML = Math.round(temperature);
+
+  let timeElement = document.querySelector("#current-time");
+  let timeStamp = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(timeStamp);
+
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windSpeedElement = document.querySelector("#windspeed");
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function formatDate(timeStamp) {
+  let minutes = timeStamp.getMinutes();
+  let hours = timeStamp.getHours();
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuseday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Satirday",
+  ];
+
+  let day = weekdays[timeStamp.getDay()];
+  return `${day} ${hours}:${minutes}`;
+
+  if (minutes > 10) {
+    minutes = `0${minutes}`;
+  }
 }
 
 function searchCityName(city) {
